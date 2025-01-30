63b46b3b156ea0a4dacfdb4c4853377c3234ed39
@ArtemMikhalev <mikhalevardm@gmail.com>
2025-01-30 22:47:50 +0300

    use .env in vite config

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 });
